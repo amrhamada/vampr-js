a02400dbc023e29954823019b075ce722b0ca5cc
Amro Abdelaziz <amr.hamada93@gmail.com>
2020-04-17 20:23:49 +0000

    comment

--- vampire.js
+++ vampire.js
@@ -10,24 +10,37 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let count = 0;
+    let currVamp = this;
+    while(currVamp.creator) {
+      currVamp = currVamp.creator;
+      count ++;
+    }
+    return count;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    return this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal;
   }
 
+  sameLevel(vampire) {
+    if (this.numberOfVampiresFromOriginal === vampire.numberOfVampiresFromOriginal) {
+      return true;
+    }
+    return false;
+  }
   /** Stretch **/
 
   // Returns the closest common ancestor of two vampires.
@@ -36,7 +49,32 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
-
+    if (!this.creator) {
+      return this;
+    }
+    if (vampire === this) {
+       return this
+    }
+    if (this.creator === vampire.creator) {
+      return this.creator;
+    }
+    if (this.creator === vampire || vampire.creator === this) {
+      return this.creator === vampire ? vampire : this;
+    } 
+    let junior = this.isMoreSeniorThan(vampire) ? vampire : this;
+    let senior = this.isMoreSeniorThan(vampire) ? this: vampire;
+    while (junior.creator) {
+      if(senior.sameLevel(junior)) {
+        while (junior.creator) {
+          if ( junior.creator === senior.creator) {
+            return junior.creator;
+          }
+          junior = junior.creator;
+          senior = senior.creator;
+        }
+      }
+      junior = junior.creator;
+    }
   }
 }
 
